890796ac36100279263a7e2c70a6440ec4318600
aswanthabam <aswanth.abam@gmail.co>
2022-11-08 14:31:57 +0530

    h

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~
 mkdir tetfiles && cd tetfiles
-curl https://github.com/aswanthabam/Termux-Executer/blob/main/script
+curl https://raw.githubusercontent.com/aswanthabam/Termux-Executer/main/script > script
 chmod +x ./script && cp ./script /data/data/com.termux/files/usr/bin/run
 echo "Installed successfully: run 'run' to start"
 echo "Help:-"
